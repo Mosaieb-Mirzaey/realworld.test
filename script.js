b9a9f9da6ec062665b8513bd5b029751f8a51f74
Mosaieb <mosaieb.mirzaey@gmail.com>
2020-07-16 19:21:52 +0430

    test server 2

--- script.js
+++ script.js
@@ -265,8 +265,124 @@ var favoriteBtnColorAll , pageI;
     await getParamUrl();
     await listGlobal();
 
-    if (await getCurrentUser()){
-        if (users.user.username !== ''){
+
+
+
+    //#########################################################  Global Articles  #############################################################
+
+    let artGlobalFrame =document.querySelector('.artGlobalFrame');
+
+    numOfGlobalArt = Object.entries(globalArticleInfo.articles);
+    console.log(globalArticleInfo.articles);
+    var globalArticleNum = numOfGlobalArt.length;
+
+
+    //########################  ساختار تکرار برای لیست مقاله های گلوبال  #####################
+
+    for (c = 0 ; c < globalArticleNum ; c++) {
+
+        //   #################    timeArticleCreated   ###############
+        let timeCreated = globalArticleInfo.articles[c].createdAt;
+        let format = "yyyy-MM-dd'T'HH:mm:ss+SS:ZZ";
+        let time = Date.parse(timeCreated, format);
+        let myDate = new Date(time);
+        let timeArticle = myDate.toDateString();
+        //*************    End timeArticleCreated     *************
+
+
+        //  ############   favoriteBtnColor    ##############
+
+        favoriteBtnColorAll = globalArticleInfo.articles[c].favorited ? 'btn-primary' : 'btn-outline-primary';
+
+        //  -------------   End favoriteBtnColor  --------------
+
+
+
+        artGlobalFrame.innerHTML += `
+
+
+                   <div class="article-preview">
+                        <div class="article-meta">
+                            <a href="https://mosaieb-mirzaey.github.io/realworld.test/html/profile.html" class="router-link-exact-active router-link-active">
+                                <img src="${globalArticleInfo.articles[c].author.image}"></a>
+                            <div class="info">
+                            <a href="https://mosaieb-mirzaey.github.io/realworld.test/html/profile.html?author=${globalArticleInfo.articles[c].author.username}">
+                                ${globalArticleInfo.articles[c].author.username}
+                            </a>
+                            <span class="date">${timeArticle}</span>
+                            </div>
+                            <button onclick="favoritePost('${globalArticleInfo.articles[c].slug}' , '${c}')" class="btn ${favoriteBtnColorAll} btn-sm pull-xs-right favoriteBtn" ">
+                                <i class="ion-heart"></i><span class="counter"> ${globalArticleInfo.articles[c].favoritesCount} </span>
+                            </button>
+                        </div>
+                        <a href="https://mosaieb-mirzaey.github.io/realworld.test/html/articles.html?slug=${globalArticleInfo.articles[c].slug}" class="preview-link">
+                            <h1>${globalArticleInfo.articles[c].title}</h1>
+                            <p>${globalArticleInfo.articles[c].description}</p>
+                            <span>Read more...</span>
+                            <ul class="tag-list">
+                            </ul>
+                        </a>
+                   </div> 
+
+        `;
+
+
+        //  ############   Article Tags    ##############
+        let tag = Object.entries(globalArticleInfo.articles[c].tagList);
+        let tagList = document.querySelectorAll(".tag-list")[c];
+        var listGlobalINum = c;
+        for (d = 0 ; d < tag.length ; d++){
+            var tagHtml = `<li class="tag-default tag-pill tag-outline"><span>${globalArticleInfo.articles[listGlobalINum].tagList[d]}</span></li>`
+            tagList.innerHTML += tagHtml;
+        }
+        // -------------   End Article Tags  -------------
+
+
+    }
+    //-------------------------  پایان ساختار تکرار برای لیست مقاله های گلوبال  ----------------------
+
+
+    //  ############ ************  Global Tags   ************ ##############
+    await tagPopularGet();
+
+    numOfGlobalTag = Object.entries(tagGlobal.tags);
+    let tagNum = numOfGlobalTag.length;
+
+    for (let i = 0 ; i < tagNum ; i++){
+        let popularTag = document.querySelector(".popularTag");
+        popularTag.innerHTML += `
+            <a href="" class="tag-pill tag-default">${tagGlobal.tags[i]}</a>
+        `;
+    }
+
+    // ------------- ***********  End Global Tags  ************ -------------
+
+
+//--------------------------------------------------------------  End Global Articles  -------------------------------------------------------------------
+
+    await getParamUrl();
+    await paginate();
+    let pagination = document.querySelector(".pagination");
+    let pageNumLength = infoPage.pages.length;
+    for (let i = 0 ; i < pageNumLength ; i++){
+        pagination.innerHTML += `
+           <li data-test="page-link-1" class="page-item">
+               <a href="https://mosaieb-mirzaey.github.io/realworld.test/?page=${infoPage.pages[i]}" class="page-link pageI">${infoPage.pages[i]}</a>
+           </li>
+            `;
+
+        pageI = document.querySelectorAll(".pageI")[i];
+
+        if (Number(pageI.textContent) == getUrl){
+            pageI.parentElement.classList.add("active");
+        }else if (window.location.href == "https://mosaieb-mirzaey.github.io/realworld.test?page=1"){
+            document.querySelectorAll(".pageI")[0].parentElement.classList.add("active");
+        }
+    }
+
+
+
+        if ((await getCurrentUser()) && users.user.username !== ''){
             document.querySelector(".loading").style.display="initial";
             document.querySelector(".listNav").innerHTML = `
                 <li class="nav-item">
@@ -407,120 +523,7 @@ var favoriteBtnColorAll , pageI;
 
             document.querySelector(".loading").style.display="none";
         }
-    }else {
-        //#########################################################  Global Articles  #############################################################
 
-        let artGlobalFrame =document.querySelector('.artGlobalFrame');
-
-        numOfGlobalArt = Object.entries(globalArticleInfo.articles);
-        console.log(globalArticleInfo.articles);
-        var globalArticleNum = numOfGlobalArt.length;
-
-
-        //########################  ساختار تکرار برای لیست مقاله های گلوبال  #####################
-
-        for (c = 0 ; c < globalArticleNum ; c++) {
-
-            //   #################    timeArticleCreated   ###############
-            let timeCreated = globalArticleInfo.articles[c].createdAt;
-            let format = "yyyy-MM-dd'T'HH:mm:ss+SS:ZZ";
-            let time = Date.parse(timeCreated, format);
-            let myDate = new Date(time);
-            let timeArticle = myDate.toDateString();
-            //*************    End timeArticleCreated     *************
-
-
-            //  ############   favoriteBtnColor    ##############
-
-            favoriteBtnColorAll = globalArticleInfo.articles[c].favorited ? 'btn-primary' : 'btn-outline-primary';
-
-            //  -------------   End favoriteBtnColor  --------------
-
-
-
-            artGlobalFrame.innerHTML += `
-
-
-                   <div class="article-preview">
-                        <div class="article-meta">
-                            <a href="https://mosaieb-mirzaey.github.io/realworld.test/html/profile.html" class="router-link-exact-active router-link-active">
-                                <img src="${globalArticleInfo.articles[c].author.image}"></a>
-                            <div class="info">
-                            <a href="https://mosaieb-mirzaey.github.io/realworld.test/html/profile.html?author=${globalArticleInfo.articles[c].author.username}">
-                                ${globalArticleInfo.articles[c].author.username}
-                            </a>
-                            <span class="date">${timeArticle}</span>
-                            </div>
-                            <button onclick="favoritePost('${globalArticleInfo.articles[c].slug}' , '${c}')" class="btn ${favoriteBtnColorAll} btn-sm pull-xs-right favoriteBtn" ">
-                                <i class="ion-heart"></i><span class="counter"> ${globalArticleInfo.articles[c].favoritesCount} </span>
-                            </button>
-                        </div>
-                        <a href="https://mosaieb-mirzaey.github.io/realworld.test/html/articles.html?slug=${globalArticleInfo.articles[c].slug}" class="preview-link">
-                            <h1>${globalArticleInfo.articles[c].title}</h1>
-                            <p>${globalArticleInfo.articles[c].description}</p>
-                            <span>Read more...</span>
-                            <ul class="tag-list">
-                            </ul>
-                        </a>
-                   </div> 
-
-        `;
-
-
-            //  ############   Article Tags    ##############
-            let tag = Object.entries(globalArticleInfo.articles[c].tagList);
-            let tagList = document.querySelectorAll(".tag-list")[c];
-            var listGlobalINum = c;
-            for (d = 0 ; d < tag.length ; d++){
-                var tagHtml = `<li class="tag-default tag-pill tag-outline"><span>${globalArticleInfo.articles[listGlobalINum].tagList[d]}</span></li>`
-                tagList.innerHTML += tagHtml;
-            }
-            // -------------   End Article Tags  -------------
-
-
-        }
-        //-------------------------  پایان ساختار تکرار برای لیست مقاله های گلوبال  ----------------------
-
-
-        //  ############ ************  Global Tags   ************ ##############
-        await tagPopularGet();
-
-        numOfGlobalTag = Object.entries(tagGlobal.tags);
-        let tagNum = numOfGlobalTag.length;
-
-        for (let i = 0 ; i < tagNum ; i++){
-            let popularTag = document.querySelector(".popularTag");
-            popularTag.innerHTML += `
-            <a href="" class="tag-pill tag-default">${tagGlobal.tags[i]}</a>
-        `;
-        }
-
-        // ------------- ***********  End Global Tags  ************ -------------
-
-
-//--------------------------------------------------------------  End Global Articles  -------------------------------------------------------------------
-
-        await getParamUrl();
-        await paginate();
-        let pagination = document.querySelector(".pagination");
-        let pageNumLength = infoPage.pages.length;
-        for (let i = 0 ; i < pageNumLength ; i++){
-            pagination.innerHTML += `
-           <li data-test="page-link-1" class="page-item">
-               <a href="https://mosaieb-mirzaey.github.io/realworld.test/?page=${infoPage.pages[i]}" class="page-link pageI">${infoPage.pages[i]}</a>
-           </li>
-            `;
-
-            pageI = document.querySelectorAll(".pageI")[i];
-
-            if (Number(pageI.textContent) == getUrl){
-                pageI.parentElement.classList.add("active");
-            }else if (window.location.href == "https://mosaieb-mirzaey.github.io/realworld.test?page=1"){
-                document.querySelectorAll(".pageI")[0].parentElement.classList.add("active");
-            }
-        }
-
-    }
 })();
 
 //   ---------------------------    General     --------------------------------
